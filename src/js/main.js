a41a5d94c5649b89c514af318c3ae56baa70b25f
victor tung <vtfsdev@gmail.com>
2020-10-09 16:57:35 -0500

    added triangles

--- src/js/main.js
+++ src/js/main.js
@@ -14,38 +14,40 @@ var draw = (function() {
       //Create the initial bounding rectangle
       rect = canvas.getBoundingClientRect(),
   
-      //current x,y
-        x=0,
-        y=0,
-
-        //starting x,y
-        x1=0, 
-        y1=0,
-
-        //ending x,y
-        x2=0,
-        y2=0,
-
-        //Tracks the last x,y state
-        lx = false,
-        ly = false,
-
-        //What shape are we drawing?
-        shape='';
+      //current x,y position
+      x=0,
+      y=0,
   
-        isDrawing=false;
-
+      //starting x,y
+      x1=0,
+      y1=0,
+  
+      //ending x,y
+      x2=0,
+      y2=0,
+  
+      //Tracks the last x,y state
+      lx = false,
+      ly = false,
+  
+      //What shape are we drawing?
+      shape='',
+  
+      //Are we drawimg a path?
+      isDrawing=false;
+  
     return {
+  
       //Set the x,y coords based on current event data
       setXY: function(evt) {
-
-            //Track the last x,y position before setting the current position.
-            lx=x;
-            ly=y;
-        
-            //Set the current x,y position
-            x = (evt.clientX - rect.left) - canvas.offsetLeft;
-            y = (evt.clientY - rect.top) - canvas.offsetTop;
+  
+        //Track last x,y position before setting the current posiiton.
+        lx=x;
+        ly=y;
+  
+        //Set the current x,y position
+        x = (evt.clientX - rect.left) - canvas.offsetLeft;
+        y = (evt.clientY - rect.top) - canvas.offsetTop;
       },
   
       //Write the x,y coods to the target div
@@ -54,58 +56,71 @@ var draw = (function() {
         document.getElementById('trackY').innerHTML = 'Y: ' + y;
       },
   
+      //Set the x1,y1
       setStart: function() {
         x1=x;
         y1=y;
       },
-      
+  
+      //Set the x2,y2
       setEnd: function() {
         x2=x;
         y2=y;
       },
-
+  
       //Sets the shape to be drawn
-        setShape: function(shp) {
-            shape = shp;
-        },
-
-        setIsDrawing: function(bool) {
-            isDrawing = bool;
-          },
-          
-          getIsDrawing: function() {
-            return isDrawing;
-          },
-
-        //Draws the selected shape
-       //Draws the selected shape
-        draw: function() {
-            ctx.restore();
-            if(shape==='rectangle')
-            {
-            this.drawRect();
-            } else if(shape==='line') {
-            this.drawLine();
-            } else if(shape==='circle') {
-            this.drawCircle();
-            } else if(shape==='path') {
-            this.drawPath();
-            } else {
-            alert('Please choose a shape');
-            }
-            ctx.save();
-        },
-
-      //Draw a rectangle
-      drawRect: function(x,y,h,w) {
-
-        //Start by using random fill colors.
-        ctx.fillStyle = '#'+Math.floor(Math.random()*16777215).toString(16);
-      
-        ctx.fillRect (x1,y1,(x2-x1),(y2-y1));
-      
+      setShape: function(shp) {
+        shape = shp;
       },
-
+  
+      getShape: function() {
+        return shape;
+      },
+  
+      setIsDrawing: function(bool) {
+        isDrawing = bool;
+      },
+  
+      getIsDrawing: function() {
+        return isDrawing;
+      },
+  
+      //Draws the selected shape
+      draw: function() {
+        ctx.restore();
+        if(shape==='rectangle')
+        {
+          this.drawRect();
+        } else if( shape==='line' ) {
+          this.drawLine();
+        } else if( shape==='path' ) {
+          this.drawPath();
+        } else if( shape==='circle' ) {
+          this.drawCircle();
+        } else if( shape==='triangle' ) {     /* LAB: Can you figure out how to draw a triangle? */
+            this.drawTriangle();    
+        } else {
+          alert('Please choose a shape');
+        }
+        ctx.save();
+      },
+  
+      //Draw a circle
+      drawCircle: function() {
+  
+        ctx.strokeStyle = '#'+Math.floor(Math.random()*16777215).toString(16);
+        ctx.fillStyle = '#'+Math.floor(Math.random()*16777215).toString(16);
+  
+        let a = (x1-x2)
+        let b = (y1-y2)
+        let radius = Math.sqrt( a*a + b*b );
+  
+        ctx.beginPath();
+        ctx.arc(x1, y1, radius, 0, 2*Math.PI);
+        ctx.stroke();
+        ctx.fill();
+      },
+  
       //Draw a line
       drawLine: function() {
         //Start by using random fill colors.
@@ -116,45 +131,45 @@ var draw = (function() {
         ctx.stroke();
       },
   
-      //Draw a circle
-      drawCircle: function() {
-
+      //Draw a path
+      drawPath: function() {
+        //console.log({x1:x,y1:y,x2:x2,y2:y2});
+        //Start by using random fill colors.
         ctx.strokeStyle = '#'+Math.floor(Math.random()*16777215).toString(16);
-        ctx.fillStyle = '#'+Math.floor(Math.random()*16777215).toString(16);
-    
-        let a = (x1-x2)
-        let b = (y1-y2)
-        let radius = Math.sqrt( a*a + b*b );
-    
         ctx.beginPath();
-        ctx.arc(x1, y1, radius, 0, 2*Math.PI);
+        ctx.moveTo(lx, ly);
+        ctx.lineTo(x, y);
         ctx.stroke();
+      },
+  
+      //Draw a rectangle
+      drawRect: function() {
+        //Start by using random fill colors.
+        ctx.fillStyle = '#'+Math.floor(Math.random()*16777215).toString(16);
+        ctx.fillRect (x1,y1,(x2-x1),(y2-y1));
+      },
+
+      /* LAB: Can you figure out how to draw a triangle? */
+      drawTriangle: function() {
+        //Start by using random fill colors.
+        ctx.fillStyle = '#'+Math.floor(Math.random()*16777215).toString(16);
+        ctx.beginPath();
+        ctx.moveTo(x1, y1);
+        ctx.lineTo(x2, y2);
+        ctx.lineTo(Math.random()*x1, Math.random()*y1);
         ctx.fill();
       },
 
-        drawPath: function() {
-            //Start by using random fill colors.
-            ctx.strokeStyle = '#'+Math.floor(Math.random()*16777215).toString(16);
-            ctx.beginPath();
-            ctx.moveTo(lx, ly);
-            ctx.lineTo(x, y);
-            ctx.stroke();
-        },
-
-        getShape: function() {
-            return shape;
-          },
-
-        getCanvas: function(){
+      getCanvas: function(){
         return canvas;
-        },
-      
+      },
+  
       //Initialize the object, this must be called before anything else
       init: function() {
         canvas.width = mWidth;
         canvas.height = mHeight;
         document.querySelector('main').appendChild(canvas);
-  
+        this.writeXY();   /* LAB: A value of 0 should load the the trackX and trackY elements on page render. */
       }
     };
   
@@ -174,30 +189,38 @@ var draw = (function() {
     }
   }, false);
   
+  //Add a mousedown listener to the canvas
   //Set the starting position
   draw.getCanvas().addEventListener('mousedown', function() {
     draw.setStart();
     draw.setIsDrawing(true);
   }, false);
-
+  
+  //Add a mouseup listener to the canvas
+  //Set the end position and draw the rectangle
   draw.getCanvas().addEventListener('mouseup', function() {
     draw.setEnd();
-    draw.setIsDrawing(true);
     draw.draw();
+    draw.setIsDrawing(false);
   }, false);
-
-  document.getElementById('btnRect').addEventListener('click',function(){
-    draw.setShape('rectangle');
+  
+  document.getElementById('btnRect').addEventListener('click', function(){
+      draw.setShape('rectangle');
   }, false);
-
-  document.getElementById('btnLine').addEventListener('click',function(){
-    draw.setShape('line');
+  
+  document.getElementById('btnLine').addEventListener('click', function(){
+      draw.setShape('line');
   }, false);
-
-  document.getElementById('btnCircle').addEventListener('click',function(){
-    draw.setShape('circle');
+  
+  document.getElementById('btnCircle').addEventListener('click', function(){
+      draw.setShape('circle');
   }, false);
-
+  
+  /* LAB: Can you figure out how to draw a triangle? */
+  document.getElementById('btnTriangle').addEventListener('click', function(){
+      draw.setShape('triangle');
+  }, false);
+  
   document.getElementById('btnPath').addEventListener('click', function(){
-    draw.setShape('path');
+      draw.setShape('path');
   }, false);
